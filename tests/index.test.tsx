6b76a60391b71262f8a73bf82ef05b2a99190bdc
二货机器人 <smith3816@gmail.com>
2022-09-26 22:31:16 +0800

    chore: fix lint

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Portal from '../src/React/Portal';
+import Portal from '../src';
 
 describe('Portal', () => {
   it('Order', () => {
