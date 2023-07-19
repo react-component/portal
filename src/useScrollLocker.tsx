c91343f848c490936e5e7170ec6a15bfb4373383
二货机器人 <smith3816@gmail.com>
2023-07-19 17:55:49 +0800

    fix: webkit usage

--- src/useScrollLocker.tsx
+++ src/useScrollLocker.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import { updateCSS, removeCSS } from 'rc-util/lib/Dom/dynamicCSS';
 import useLayoutEffect from 'rc-util/lib/hooks/useLayoutEffect';
-import getScrollBarSize from 'rc-util/lib/getScrollBarSize';
+import { getTargetScrollBarSize } from 'rc-util/lib/getScrollBarSize';
 import { isBodyOverflowing } from './util';
 
 const UNIQUE_ID = `rc-util-locker-${Date.now()}`;
 
 let uuid = 0;
 
-export default function useScrollLocker(
-  lock?: boolean,
-) {
+export default function useScrollLocker(lock?: boolean) {
   const mergedLock = !!lock;
   const [id] = React.useState(() => {
     uuid += 1;
@@ -19,7 +17,7 @@ export default function useScrollLocker(
 
   useLayoutEffect(() => {
     if (mergedLock) {
-      const scrollbarSize = getScrollBarSize();
+      const scrollbarSize = getTargetScrollBarSize(document.body).width;
       const isOverflow = isBodyOverflowing();
 
       updateCSS(
