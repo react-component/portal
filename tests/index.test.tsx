e935ff68c029b65741adced6e5115bbc821480d8
afc163 <afc163@gmail.com>
2026-07-01 14:45:46 +0800

    chore: fix upgraded tooling checks

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -78,11 +78,11 @@ describe('Portal', () => {
       };
 
       const Demo = () => {
-        const divRef = React.useRef();
+        const divRef = React.useRef<HTMLDivElement>(null);
 
         return (
           <div ref={divRef} className="holder">
-            <Portal open getContainer={() => divRef.current}>
+            <Portal open getContainer={() => divRef.current!}>
               <Content />
             </Portal>
           </div>
@@ -239,7 +239,7 @@ describe('Portal', () => {
     let checked = false;
 
     const Demo = ({ open }: { open?: boolean }) => {
-      const pRef = React.useRef();
+      const pRef = React.useRef<HTMLParagraphElement>(null);
 
       React.useEffect(() => {
         if (open) {
